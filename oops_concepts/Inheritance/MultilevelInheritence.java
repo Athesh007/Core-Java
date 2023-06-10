2547a61a5465e3a783a7cb3c721145ef7b4f256e
Athesh007 <kavipprasath80@gmail.com>
2023-06-10 10:38:10 +0530

    Update MultilevelInheritence.java

--- oops_concepts/Inheritance/MultilevelInheritence.java
+++ oops_concepts/Inheritance/MultilevelInheritence.java
@@ -1,41 +1,38 @@
 package oops_concepts.Inheritance;
 
-class A{
-    A(){
+class A {
+    A() {
         System.out.println("A's Constructor");
     }
-    void methodA()
-    {
+
+    void methodA() {
         System.out.println("A's Method");
     }
 }
 
-class B extends A{
-    B()
-    {
+class B extends A {
+    B() {
         System.out.println("B's Constructor");
     }
-    void methodB()
-    {
+
+    void methodB() {
         System.out.println("B's Method");
     }
-
 }
 
-class C extends B{
-    C(){
-       System.out.println("C's Constructor"); 
+class C extends B {
+    C() {
+        System.out.println("C's Constructor");
     }
-    void methodC()
-    {
+
+    void methodC() {
         System.out.println("C's Method");
     }
-
 }
 
-class MultilevelInheritence{
+class MultilevelInheritance {
     public static void main(String[] args) {
-        C c=new C();
+        C c = new C();
         c.methodA();
         c.methodB();
         c.methodC();
